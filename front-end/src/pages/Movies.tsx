49900e619fe9b6239a60b181aee986cc6a18383b
bimalmagar10 <bimalmagar873@gmail.com>
2023-03-02 00:48:20 +0545

    finish layout design

---
+++ front-end/src/pages/Movies.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactElement } from "../lib/types";
+
+const Movies:FC =  ():ReactElement => {
+    return (
+        <>
+            This is movies page
+        </>
+    );
+};
+
+export default Movies;
