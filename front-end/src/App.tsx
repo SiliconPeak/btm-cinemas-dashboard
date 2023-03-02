cd3d1157e28ab0ad5eb9074ce2671f5ce2494715
bimalmagar10 <bimalmagar873@gmail.com>
2023-03-03 01:22:25 +0545

    finish user crud form and list design

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,10 +3,14 @@ import Layout from "./components/Layout";
 import Movies from "./pages/Movies";
 import User from "./pages/User";
 import './App.css';
+import { NavigateFunction } from "./lib/types";
+import { useEffect } from "react";
+import UserForm from "./components/UserForm";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
       <Route index element={<User/>}/>
+      <Route path="/users/create" element={<UserForm/>}/>
       <Route path="/movies" element={<Movies/>}/>
     </Route>
   )
--- front-end/src/pages/User/index.tsx
+++ front-end/src/pages/User/index.tsx
@@ -1,6 +1,8 @@
+import {useState} from "react";
 import { FC, ReactElement } from "../../lib/types";
 import { Typography,Button } from "antd";
-import { SearchOutlined, UserAddOutlined } from "@ant-design/icons";
+import { UserAddOutlined } from "@ant-design/icons";
+import { Link} from "react-router-dom";
 import "./User.css";
 import Table from "../../components/Table";
 const {Title} = Typography;
@@ -9,7 +11,9 @@ const User:FC = ():ReactElement => {
         <div className="user__settings">
             <div className="user__settings-header">
                 <Title level={4}>Users List</Title>
-                <Button type="primary" icon={<UserAddOutlined style={{fontSize:"18px"}}/>}>Add User</Button>
+                <Button type="primary" icon={<UserAddOutlined style={{fontSize:"18px"}}/>}>
+                    <Link to="/users/create">Add User</Link>
+                </Button>
             </div>
             <Table/>
         </div>
